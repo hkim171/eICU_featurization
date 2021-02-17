bb3006e9e3b0b4232cebf88106f1b70528700625
hkim171 <hkim171@jhu.edu>
2021-02-17 14:57:23 -0500

    package install script test

---
+++ download_packages.R
@@ -0,0 +1,30 @@
+#download packages in R
+
+#make sure use following commands in terminal within MARCC.
+#ml R/3.6.1
+#ml gcc/5.5
+
+#run this script with Rscript download_packages.R
+
+load_packages <- function(package_list) {
+  #   Checks if given library is installed. If it is not, installs and loads it.
+  # 	If it is, loads it.
+  # 
+  # 	Args:
+  # 	library_list: A list of vectors containing strings of the required
+  # 					packages
+  # 	Returns:
+  # 	Null
+  
+  for (pkg in package_list) {
+    if (!require(pkg, character.only=TRUE)) {
+      install.packages(pkg)
+      library(pkg, character.only = TRUE)
+    }
+  }
+}
+
+package_list <- c("tidyverse", "data.table", "ranger", "randomForest", "caret", "MLmetrics", "Metrics", 
+                  "EvaluationMeasures", "Rmisc", "xgboost", "glmnet", "randomForestSRC", "caretEnsemble")
+
+load_packages(package_list)
